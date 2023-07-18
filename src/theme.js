05048739a9d767b0741bb454c49bc1bde7403402
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-07-18 21:42:37 +0200

    font and theme added

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    whisper: "#F5F5FA",
+    mystic: "#E4E6F0",
+    waterloo: "#7E839A",
+    woodsmoke: "#18181B",
+    darkerGray:"#7E839A",
+    pattensBlue: "#D6E4FF",
+  },
+};

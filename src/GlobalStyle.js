4041b9c8cdce8a0d73c40c723c986ab5c50fb339
Weronika Chmielewska <98229283+WerkaStrong@users.noreply.github.com>
2023-07-18 20:02:30 +0200

    created GlobalStyle.js

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        max-width: 1940px;
+        margin: 0 auto;
+        font-family: 'Poppins', sans-serif;
+        color: ${({theme}) => theme.color.woodsmoke};
+        background: ${({theme}) => theme.color.whisper};
+    }
+`;

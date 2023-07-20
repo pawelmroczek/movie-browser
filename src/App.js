0583158949c79d3125474e5cd628eede6aaf1481
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-07-20 19:36:03 +0200

    basic routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import { ThemeProvider } from "styled-components";
 import Input from "./common/Navigation/Input";
 import { theme } from "./theme";
+import {
+  HashRouter,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Input />
+      <HashRouter>
+        {/* here is a place for navigation with my input inside of it */}
+        <Input />
+        <Switch>
+          <Route path="/movies">movies</Route>
+          <Route path="/people">people</Route>
+          <Route path="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Switch>
+      </HashRouter>
     </ThemeProvider>
   );
 }

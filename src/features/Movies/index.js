624a9ff4c236af30c1ab1e9441a7255ac41037f7
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-07-24 21:58:55 +0200

    make title change depends of data and query

--- src/features/Movies/index.js
+++ src/features/Movies/index.js
@@ -1,24 +1,63 @@
+import { useSelector } from "react-redux";
 import { Container, Content, Title } from "../../common/Container";
 import { Pagination } from "../../common/Pagination";
 import { Tile } from "../../common/Tile";
+import { useQueryParameter } from "../../queryParameters";
+import searchQueryParamNam from "../../searchQueryParamName";
+import { selectData } from "../browserSlice";
 
-const Movies = () => (
-  <>
-    <Container>
-      <Title>Popular movies</Title>
-      <Content>
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-        <Tile />
-      </Content>
-    </Container>
-    <Pagination />
-  </>
-);
+const Movies = () => {
+  const query = useQueryParameter(searchQueryParamNam);
+  const data = useSelector(selectData);
+  console.log(data);
+
+  if (!data.length && query) {
+    return (
+      <Container>
+        <Title>Sorry, there are no results for "{query}"</Title>
+      </Container>
+    );
+  } else if (query) {
+    return (
+      <>
+        <Container>
+          <Title>
+            Search results for "{query}" ({data.length})
+          </Title>
+          <Content>
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+          </Content>
+        </Container>
+        <Pagination />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Container>
+          <Title>Popular movies</Title>
+          <Content>
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+            <Tile />
+          </Content>
+        </Container>
+        <Pagination />
+      </>
+    );
+  }
+};
 
 export default Movies;

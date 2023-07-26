46271914eda1f45b3597477397975dea7539588c
Weronika Chmielewska <98229283+WerkaStrong@users.noreply.github.com>
2023-07-26 21:31:35 +0200

    check if is movie

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -4,29 +4,31 @@ import star from "../images/Vector.svg";
 import customPoster from "../images/Video.svg"
 
 export const Tile = ({ movie }) => {
+    if (!movie) {
+        return null; // return null immediately if movie is undefined
+    }
+
     const posterUrl = movie.poster_path
         ? `https://image.tmdb.org/t/p/original${movie.poster_path}`
         : customPoster;
         
     return (
-        movie ? (
-            <StyledTile>
-                <Poster>
-                    <ImagePoster src={posterUrl} alt="" isCustom={posterUrl === customPoster}></ImagePoster>
-                </Poster>
-                <TileContent>
-                    <Title>{movie.title}</Title>
-                    <Year>{movie.relase_date}</Year>
-                    <Tags>
-                        <Tag></Tag>
-                    </Tags>
-                    <Rating>
-                        <StarImage src={star} alt=""></StarImage>
-                        <Rate>{movie.vote_average}</Rate>
-                        <Votes>{movie.vote_count} votes</Votes>
-                    </Rating>
-                </TileContent>
-            </StyledTile>
-        ) : null
+        <StyledTile>
+            <Poster>
+                <ImagePoster src={posterUrl} alt="" isCustom={posterUrl === customPoster}></ImagePoster>
+            </Poster>
+            <TileContent>
+                <Title>{movie.title}</Title>
+                <Year>{movie.release_date}</Year>
+                <Tags>
+                    <Tag></Tag>
+                </Tags>
+                <Rating>
+                    <StarImage src={star} alt=""></StarImage>
+                    <Rate>{movie.vote_average}</Rate>
+                    <Votes>{movie.vote_count} votes</Votes>
+                </Rating>
+            </TileContent>
+        </StyledTile>
     )
 };

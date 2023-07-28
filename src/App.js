16c7f03c871b9cc148cead481f7b1160200349ac
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-07-28 16:28:49 +0200

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React from "react";
-import { Navigation } from "./common/Navigation";
-import Movies from "./features/Movies";
-import {
-  HashRouter,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom/cjs/react-router-dom";
-import People from "./features/People";
-import { toMovies, toPeople } from "./routes";
+import React from 'react'
+import { Navigation } from './common/Navigation'
+import Movies from './features/Movies'
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom'
+import People from './features/People'
+import { toMovies, toPeople } from './routes'
 
 function App() {
-  return (
-    <HashRouter>
-      <Navigation />
-      <Switch>
-        <Route path={toMovies()}>
-          <Movies />
-        </Route>
-        <Route path={toPeople()}>
-          <People/>
-        </Route>
-        <Route path="/">
-          <Redirect to={toMovies()} />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
+	return (
+		<HashRouter>
+			<Navigation />
+			<Switch>
+				<Route path={toMovies()}>
+					<Movies />
+				</Route>
+				<Route path={toPeople()}>
+					<People />
+				</Route>
+				<Route path='/'>
+					<Redirect to={toMovies()} />
+				</Route>
+			</Switch>
+		</HashRouter>
+	)
 }
 
-export default App;
+export default App

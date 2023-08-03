2dbe82f4e7d9bce0967009c5169d66a1a1a8fc0c
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-03 16:07:47 +0200

    pagination mechanism added

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,43 +1,54 @@
-import React from "react";
-import { StyledTags, FirstVector, PageNumber, StyledPagination, GrayTag, BlueTag, Number, Text, SecondVector } from "./styled";
+import {
+  StyledTags,
+  FirstVector,
+  PageNumber,
+  StyledPagination,
+  Tag,
+  Number,
+  Text,
+  SecondVector,
+} from "./styled";
 import lessThan from "../images/lessThan.svg";
 import greaterThan from "../images/greaterThan.svg";
+import usePagination from "./usePagination";
 
+export const Pagination = () => {
+  const {
+    page,
+    totalPages,
+    pageDecrease,
+    pageIncrease,
+    setFirstPage,
+    setLastPage,
+  } = usePagination();
 
-export const Pagination = () => (
+  return (
     <StyledPagination>
-        <StyledTags>
-            <GrayTag
-                to="/first"
-            >
-                <FirstVector src={lessThan} alt="" />
-                <SecondVector src={lessThan} alt="" />
-                <Text>First</Text>
-            </GrayTag>
-            <GrayTag
-                to="/previous"
-            >
-                <FirstVector src={lessThan} alt="" />
-                <Text>Previous</Text>
-            </GrayTag>
-        </StyledTags>
-        <PageNumber>
-            Page <Number>1</Number> of <Number>500</Number>
-        </PageNumber>
-        <StyledTags>
-            <BlueTag
-                to="/next"
-            >
-                <Text>Next</Text>
-                <FirstVector src={greaterThan} alt="" />
-            </BlueTag>
-            <BlueTag
-                to="/last"
-            >
-                <Text>Last</Text>
-                <FirstVector src={greaterThan} alt="" />
-                <SecondVector src={greaterThan} alt="" />
-            </BlueTag>
-        </StyledTags>
+      <StyledTags>
+        <Tag onClick={setFirstPage} disabled={page === 1}>
+          <FirstVector src={lessThan} alt="" />
+          <SecondVector src={lessThan} alt="" />
+          <Text>First</Text>
+        </Tag>
+        <Tag onClick={pageDecrease} disabled={page <= 1}>
+          <FirstVector src={lessThan} alt="" />
+          <Text>Previous</Text>
+        </Tag>
+      </StyledTags>
+      <PageNumber>
+        Page <Number>{page}</Number> of <Number>{totalPages}</Number>
+      </PageNumber>
+      <StyledTags>
+        <Tag onClick={pageIncrease} disabled={page >= totalPages}>
+          <Text>Next</Text>
+          <FirstVector src={greaterThan} alt="" />
+        </Tag>
+        <Tag onClick={setLastPage} disabled={page === totalPages}>
+          <Text>Last</Text>
+          <FirstVector src={greaterThan} alt="" />
+          <SecondVector src={greaterThan} alt="" />
+        </Tag>
+      </StyledTags>
     </StyledPagination>
-);
+  );
+};

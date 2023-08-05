aee8b625c24e0810d9ba6921393dc203cfbd3dee
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-05 15:31:56 +0200

    changed structure of code in People component

--- src/features/Movie/People/index.js
+++ src/features/Movie/People/index.js
@@ -1,46 +1,29 @@
 import React from 'react'
 import { Header, Container, Actors, Wrapper, PersonPoster, PersonTitle, StyledActorTile, Character } from './styled'
-import { useParams } from 'react-router-dom';
-import useCredits from './useCredits';
+import customPoster from '../img/Profile.svg';
 
-const People = () => {
-	const { id } = useParams();
-	const movieId = id;
-
-	const { cast, crew } = useCredits(movieId);
-
-	return (
-		<Wrapper>
-			<Container>
-				<Header>Cast</Header>
-				<Actors>
-					{cast.map((actor) => {
-						const actor_posterUrl = `https://image.tmdb.org/t/p/original${actor.profile_path}`;
-						return (
-							<StyledActorTile key={actor.id}>
-								<PersonPoster src={actor_posterUrl} alt={actor.name} />
-								<PersonTitle>{actor.name}</PersonTitle>
-								<Character>{actor.character}</Character>
-							</StyledActorTile>
-						);
-					})}
-				</Actors>
-				<Header>Crew</Header>
-				<Actors>
-					{crew.map((crewMember) => {
-						const crew_member_posterUrl = `https://image.tmdb.org/t/p/original${crewMember.profile_path}`;
-						return (
-							<StyledActorTile key={crewMember.id}>
-								<PersonPoster src={crew_member_posterUrl} alt={crewMember.name} />
-								<PersonTitle>{crewMember.name}</PersonTitle>
-								<Character>Department: {crewMember.known_for_department}</Character>
-							</StyledActorTile>
-						);
-					})}
-				</Actors>
-			</Container>
-		</Wrapper>
-	)
+const People = ({ title, people }) => {
+    return (
+        <Wrapper>
+            <Container>
+                <Header>{title}</Header>
+                <Actors>
+                    {people.map((person) => {
+                        const person_posterUrl = person.profile_path ? `https://image.tmdb.org/t/p/original${person.profile_path}` : customPoster;
+                        return (
+                            <StyledActorTile key={person.id}>
+                                <PersonPoster src={person_posterUrl} alt={person.name} />
+                                <PersonTitle>{person.name}</PersonTitle>
+                                <Character>
+                                    {title === 'Cast' ? person.character : `Department: ${person.known_for_department}`}
+                                </Character>
+                            </StyledActorTile>
+                        );
+                    })}
+                </Actors>
+            </Container>
+        </Wrapper>
+    )
 }
 
-export default People
+export default People;

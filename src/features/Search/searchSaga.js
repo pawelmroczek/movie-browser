e5245cc29d0713dd8d873503df8013a4dcffe923
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-07 15:16:48 +0200

    saga changed

--- src/features/Search/searchSaga.js
+++ src/features/Search/searchSaga.js
@@ -1,21 +1,38 @@
 import { call, put, takeEvery } from "redux-saga/effects";
-import { fetchSearchResult, setData, setStatus, setGenres, setPopularPeople, setPerson } from "../browserSlice";
+import {
+  fetchSearchResult,
+  setData,
+  setStatus,
+  setGenres,
+  fetchPopular,
+} from "../browserSlice";
 import { getSearchResults } from "./getSearchResults";
 import { getGenres } from "../../common/Genres/getGenres";
-import { getPopularPeople } from "../People/getPopularPeople";
-import { getPerson } from "./getSearchPeople";
+import { getPopular } from "../Movies/getPopularMovies";
 
-function* fetchSearchResultsHandler({ payload: object}) {
+function* fetchSearchResultsHandler({ payload: object }) {
   try {
     yield put(setStatus("loading"));
-    const data = yield call(getSearchResults, object.query, object.page);
+    const data = yield call(
+      getSearchResults,
+      object.query,
+      object.page,
+      object.destination
+    );
     yield put(setData(data));
     const genres = yield call(getGenres);
     yield put(setGenres(genres));
-    const people = yield call(getPopularPeople);
-    yield put(setPopularPeople(people));
-    const person = yield call(getPerson, object.query);
-    yield put(setPerson(person));
+    yield put(setStatus("success"));
+  } catch (error) {
+    yield put(setStatus("error"));
+  }
+}
+
+function* fetchPopularHandler({ payload: object }) {
+  try {
+    yield put(setStatus("loading"));
+    const data = yield call(getPopular, object.page, object.destination);
+    yield put(setData(data));
     yield put(setStatus("success"));
   } catch (error) {
     yield put(setStatus("error"));
@@ -24,4 +41,5 @@ function* fetchSearchResultsHandler({ payload: object}) {
 
 export function* searchSaga() {
   yield takeEvery(fetchSearchResult.type, fetchSearchResultsHandler);
+  yield takeEvery(fetchPopular.type, fetchPopularHandler);
 }

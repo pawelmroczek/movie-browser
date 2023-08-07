9e596e301358c866a26164e7e28ace65c402ed0a
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-07 15:17:01 +0200

    tile fixed

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,46 +1,52 @@
-import React from 'react'
+import React from "react";
 import {
-	LinkElement,
-	StyledTile,
-	Rating,
-	TileContent,
-	TileTitle,
-	Year,
-	Rate,
-	Votes,
-	StarImage,
-	Poster,
-	ImagePoster,
-} from './styled'
-import star from '../images/Vector.svg'
-import customPoster from '../images/Video.svg'
-import { Genres } from '../Genres'
+  LinkElement,
+  StyledTile,
+  Rating,
+  TileContent,
+  TileTitle,
+  Year,
+  Rate,
+  Votes,
+  StarImage,
+  Poster,
+  ImagePoster,
+} from "./styled";
+import star from "../images/Vector.svg";
+import customPoster from "../images/Video.svg";
+import { Genres } from "../Genres";
 
 export const Tile = ({ movie, genres }) => {
-	if (!movie) {
-		return null
-	}
+  if (!movie) {
+    return null;
+  }
 
-	const posterUrl = movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : customPoster
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/original${movie.poster_path}`
+    : customPoster;
 
-	return (
-		<StyledTile>
-			<Poster>
-				<ImagePoster src={posterUrl} alt='' $isCustom={posterUrl === customPoster}></ImagePoster>
-			</Poster>
-			<TileContent>
-				<LinkElement to={`/movies/${movie.id}`}>
-					<TileTitle>{movie.title}</TileTitle>
-				</LinkElement>
-				<Year>{movie.release_date}</Year>
+  return (
+    <StyledTile>
+      <Poster>
+        <ImagePoster
+          src={posterUrl}
+          alt=""
+          $isCustom={posterUrl === customPoster}
+        ></ImagePoster>
+      </Poster>
+      <TileContent>
+        <LinkElement to={`/movies/${movie.id}`}>
+          <TileTitle>{movie.title}</TileTitle>
+        </LinkElement>
+        <Year>{movie.release_date}</Year>
 
-				<Genres genre_ids={movie.genre_ids} genres={genres} />
-				<Rating>
-					<StarImage src={star} alt=''></StarImage>
-					<Rate>{movie.vote_average === 0 ? 0 : movie.vote_average.toFixed(1)}</Rate>
-					<Votes>{movie.vote_count} votes</Votes>
-				</Rating>
-			</TileContent>
-		</StyledTile>
-	)
-}
+        <Genres genre_ids={movie.genre_ids} genres={genres} />
+        <Rating>
+          <StarImage src={star} alt=""></StarImage>
+          <Rate>{movie.vote_average ? movie.vote_average.toFixed(1) : 0}</Rate>
+          <Votes>{movie.vote_count} votes</Votes>
+        </Rating>
+      </TileContent>
+    </StyledTile>
+  );
+};

af6e34c0c3719fb07f66573b0a5c39bb22162264
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-08-17 21:42:20 +0200

    Added API connection to actor description

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -1,99 +1,96 @@
-import styled from "styled-components";
-import { Tag, Tags } from "../Genres/styled";
+import styled from 'styled-components'
 
 export const StyledPagination = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 32px 0px 31px 0px;  
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	padding: 32px 0px 31px 0px;
 
-  @media (min-width: 768px) {
-    padding: 40px 0px 103px 0px;  
-    }
-`;
+	@media (min-width: 768px) {
+		padding: 40px 0px 103px 0px;
+	}
+`
 
 export const StyledTags = styled.ul`
-    margin: initial;
-    gap: 8px;
-    display: flex;
-    flex-direction: row;
-    list-style: none;
-    flex-wrap: wrap;
-    padding: 0;
-   
-    @media (min-width: 768px) {
-        font-size: 16px;
-    }
-`;
+	margin: initial;
+	gap: 8px;
+	display: flex;
+	flex-direction: row;
+	list-style: none;
+	flex-wrap: wrap;
+	padding: 0;
 
+	@media (min-width: 768px) {
+		font-size: 16px;
+	}
+`
 
 export const GrayTag = styled.li`
-    background: ${({ theme }) => theme.color.mystic};
-    font-size: 10px;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 8px 12px;
-    gap: 4px;
+	background: ${({ theme }) => theme.color.mystic};
+	font-size: 10px;
+	border-radius: 5px;
+	display: flex;
+	align-items: center;
+	padding: 8px 12px;
+	gap: 4px;
 
-    @media (min-width: 768px) {
-        padding: 8px 16px;
-        font-size: 14px;
-        gap: 8px;
-    }
-`;
+	@media (min-width: 768px) {
+		padding: 8px 16px;
+		font-size: 14px;
+		gap: 8px;
+	}
+`
 
 export const BlueTag = styled(GrayTag)`
-    background: ${({ theme }) => theme.color.pattensBlue};
-`;
+	background: ${({ theme }) => theme.color.pattensBlue};
+`
 
 export const FirstVector = styled.img`
-    max-width: 5px;
-    max-height: 8px;
+	max-width: 5px;
+	max-height: 8px;
 
-    @media (min-width: 768px) {
-        min-width: 7px;
-        min-height: 11px;
-    }
-`;
+	@media (min-width: 768px) {
+		min-width: 7px;
+		min-height: 11px;
+	}
+`
 
 export const SecondVector = styled(FirstVector)`
-
-    @media (min-width: 768px) {
-        display: none;
-    }
-`;
+	@media (min-width: 768px) {
+		display: none;
+	}
+`
 
 export const Text = styled.p`
-    display: none;
+	display: none;
 
-    @media (min-width: 768px) {
-        display: inline;
-        margin: initial;
-    }
+	@media (min-width: 768px) {
+		display: inline;
+		margin: initial;
+	}
 `
 
 export const PageNumber = styled.div`
-    font-size: 10px;
-    padding: 0px 8px 0px 8px;
-    color: ${({ theme }) => theme.color.waterloo};
-    display: flex;
-    flex-wrap: row;
-    align-items: center;
+	font-size: 10px;
+	padding: 0px 8px 0px 8px;
+	color: ${({ theme }) => theme.color.waterloo};
+	display: flex;
+	flex-wrap: row;
+	align-items: center;
 
-    @media (min-width: 768px) {
-        font-size: 16px;
-        padding: 0px 24px 0px 24px;
-    }
+	@media (min-width: 768px) {
+		font-size: 16px;
+		padding: 0px 24px 0px 24px;
+	}
 `
 export const Number = styled.div`
-    font-weight: 600;
-    color: ${({ theme }) => theme.color.woodsmoke};
-    padding: 2px;
+	font-weight: 600;
+	color: ${({ theme }) => theme.color.woodsmoke};
+	padding: 2px;
 
-    @media (min-width: 768px) {
-        font-size: 16px;
-        padding: 8px;
-    }
- `
+	@media (min-width: 768px) {
+		font-size: 16px;
+		padding: 8px;
+	}
+`
--- src/features/ActorPage/index.js
+++ src/features/ActorPage/index.js
@@ -1,19 +1,19 @@
-import { Content } from "../Movie/styled";
-import People from "../People";
-import ActorMainTile from "./ActorMainTile";
-import MovieCrew from "./MoviesCrew";
-import MovieCast from "./MoviesCast";
+import { Content } from '../Movie/styled'
+// import People from "../People";
+import ActorMainTile from './ActorMainTile'
+import MovieCrew from './MoviesCrew'
+import MovieCast from './MoviesCast'
 
 const ActorPage = () => {
-    return (
-        <>
-            <Content>
-                <ActorMainTile />
-            </Content>
-            <MovieCast />
-            <MovieCrew />
-        </>
-    )
+	return (
+		<>
+			<Content>
+				<ActorMainTile />
+			</Content>
+			<MovieCast />
+			<MovieCrew />
+		</>
+	)
 }
 
-export default ActorPage;
+export default ActorPage

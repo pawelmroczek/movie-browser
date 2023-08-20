f1dc216bb9273451ab7cb93b6f41bba33b09950c
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-20 21:05:09 +0200

    added genres to crew and cast

--- src/features/ActorPage/MoviesCast/index.js
+++ src/features/ActorPage/MoviesCast/index.js
@@ -8,62 +8,84 @@ import {
 	TileTitle,
 	Votes,
 	Year,
-} from '../../../common/Tile/styled'
-import customPoster from '../../../common/images/Video.svg'
-import star from '../../../common/images/Vector.svg'
-import { Container, Header, Movies, Rating, Tag, Tags, TileContent } from './styled'
-import { Wrapper } from '../../Movie/People/styled'
-import { useParams } from 'react-router-dom'
-import useCast from './useCast';
-
-const MovieCast = () => {
-	const { id } = useParams()
-	const actorId = id
-	const castData = useCast(actorId)
-
+  } from '../../../common/Tile/styled';
+  import customPoster from '../../../common/images/Video.svg';
+  import star from '../../../common/images/Vector.svg';
+  import { Container, Header, Movies, Rating, Tag, Tags, TileContent } from './styled';
+  import { Wrapper } from '../../Movie/People/styled';
+  import { useParams } from 'react-router-dom';
+  import useCast from './useCast';
+  import { useEffect, useState } from 'react';
+  import { getGenres } from '../../../common/Genres/getGenres';
+  
+  const MovieCast = () => {
+	const [genres, setGenres] = useState([]);
+  
+	useEffect(() => {
+	  const fetchGenres = async () => {
+		try {
+		  const genres = await getGenres();
+		  setGenres(genres);
+		} catch (error) {
+		  console.error('Error fetching genres:', error);
+		}
+	  };
+  
+	  fetchGenres();
+	}, []);
+  
+	const { id } = useParams();
+	const actorId = id;
+	const castData = useCast(actorId);
+  
 	return (
-		<Wrapper>
-			<Header> Movies - cast ({castData ? castData.length : 0})</Header>
-			<Container>
-				<Movies>
-					{castData &&
-						castData.map((castMember) => (
-							<StyledTile key={castMember.id}>
-								<Poster>
-									<LinkElement to={`/movies/${castMember.id}`}>
-										<ImagePoster
-											$isCustom={castMember.poster_path ? false : true}
-											src={
-												castMember.poster_path ? `https://image.tmdb.org/t/p/original${castMember.poster_path}` : customPoster
-											}
-											alt={castMember.title}
-										></ImagePoster>
-									</LinkElement>
-								</Poster>
-								<TileContent>
-									<LinkElement to={`/movies/${castMember.id}`}>
-										<TileTitle>{castMember.title}</TileTitle>
-									</LinkElement>
-									<Year>
-										({castMember.release_date ? castMember.release_date.slice(0, 4) : '-'})
-									</Year>
-									<Tags>
-										<Tag>Action</Tag>
-										<Tag>Adventure</Tag>
-										<Tag>Drama</Tag>
-									</Tags>
-									<Rating>
-										<StarImage src={star} alt=''></StarImage>
-										<Rate>{castMember.vote_average || '-'}</Rate>
-										<Votes> {castMember.vote_count ? `${castMember.vote_count} votes` : '-'}</Votes>
-									</Rating>
-								</TileContent>
-							</StyledTile>
+	  <Wrapper>
+		<Header> Movies - cast ({castData ? castData.length : 0})</Header>
+		<Container>
+		  <Movies>
+			{castData &&
+			  castData.map((castMember) => {
+				const movieGenres = genres.filter((genre) => castMember.genre_ids.includes(genre.id));
+  
+				return (
+				  <StyledTile key={castMember.id}>
+					<Poster>
+					  <LinkElement to={`/movies/${castMember.id}`}>
+						<ImagePoster
+						  $isCustom={castMember.poster_path ? false : true}
+						  src={
+							castMember.poster_path
+							  ? `https://image.tmdb.org/t/p/original${castMember.poster_path}`
+							  : customPoster
+						  }
+						  alt={castMember.title}
+						></ImagePoster>
+					  </LinkElement>
+					</Poster>
+					<TileContent>
+					  <LinkElement to={`/movies/${castMember.id}`}>
+						<TileTitle>{castMember.title}</TileTitle>
+					  </LinkElement>
+					  <Year>({castMember.release_date ? castMember.release_date.slice(0, 4) : '-'})</Year>
+					  <Tags>
+						{movieGenres.map((genre) => (
+						  <Tag key={genre.id}>{genre.name}</Tag>
 						))}
-				</Movies>
-			</Container>
-		</Wrapper>
-	)
-}
-
-export default MovieCast
+					  </Tags>
+					  <Rating>
+						<StarImage src={star} alt=""></StarImage>
+						<Rate>{castMember.vote_average || '-'}</Rate>
+						<Votes> {castMember.vote_count ? `${castMember.vote_count} votes` : '-'}</Votes>
+					  </Rating>
+					</TileContent>
+				  </StyledTile>
+				);
+			  })}
+		  </Movies>
+		</Container>
+	  </Wrapper>
+	);
+  };
+  
+  export default MovieCast;
+  
--- src/features/ActorPage/MoviesCrew/index.js
+++ src/features/ActorPage/MoviesCrew/index.js
@@ -8,61 +8,82 @@ import {
 	TileTitle,
 	Votes,
 	Year,
-} from '../../../common/Tile/styled'
-import customPoster from '../../../common/images/Video.svg'
-import star from '../../../common/images/Vector.svg'
+} from '../../../common/Tile/styled';
+import customPoster from '../../../common/images/Video.svg';
+import star from '../../../common/images/Vector.svg';
 import { Container, Header, Movies, Rating, Tag, Tags, TileContent } from './styled'
-import { Wrapper } from '../../Movie/People/styled'
-import { useParams } from 'react-router-dom'
-import useCrew from './useCrew'
+import { Wrapper } from '../../Movie/People/styled';
+import { useParams } from 'react-router-dom';
+import useCrew from './useCrew';
+import { useEffect, useState } from 'react';
+import { getGenres } from '../../../common/Genres/getGenres';
 
 const MovieCrew = () => {
+	const [genres, setGenres] = useState([]);
 	const { id } = useParams()
 	const actorId = id
 	const crewData = useCrew(actorId)
 
+	useEffect(() => {
+		const fetchGenres = async () => {
+		  try {
+			const genres = await getGenres();
+			setGenres(genres);
+		  } catch (error) {
+			console.error('Error fetching genres:', error);
+		  }
+		};
+	
+		fetchGenres();
+	  }, []);
+
 	return (
 		<Wrapper>
-			<Header> Movies - crew ({crewData ? crewData.length : 0})</Header>
-			<Container>
-				<Movies>
-					{crewData &&
-						crewData.map((crewMember, index) => (
-							<StyledTile key={index}>
-								<Poster>
-									<LinkElement to={`/movies/${crewMember.id}`}>
-										<ImagePoster
-											$isCustom={crewMember.poster_path ? false : true}
-											src={
-												crewMember.poster_path ? `https://image.tmdb.org/t/p/original${crewMember.poster_path}` : customPoster
-											}
-											alt={crewMember.title}></ImagePoster>
-									</LinkElement>
-								</Poster>
-								<TileContent>
-									<LinkElement to={`/movies/${crewMember.id}`}>
-										<TileTitle>{crewMember.title}</TileTitle>
-									</LinkElement>
-									<Year>
-										({crewMember.release_date ? crewMember.release_date.slice(0, 4) : '-'})
-									</Year>
-									<Tags>
-										<Tag>Action</Tag>
-										<Tag>Adventure</Tag>
-										<Tag>Drama</Tag>
-									</Tags>
-									<Rating>
-										<StarImage src={star} alt=''></StarImage>
-										<Rate>{crewMember.vote_average || '-'}</Rate>
-										<Votes> {crewMember.vote_count ? `${crewMember.vote_count} votes` : '-'}</Votes>
-									</Rating>
-								</TileContent>
-							</StyledTile>
-						))}
-				</Movies>
-			</Container>
+		  <Header> Movies - crew ({crewData ? crewData.length : 0})</Header>
+		  <Container>
+			<Movies>
+			  {crewData &&
+				crewData.map((crewMember) => {
+				  const movieGenres = genres.filter((genre) => crewMember.genre_ids.includes(genre.id));
+	
+				  return (
+					<StyledTile key={crewMember.id}>
+					  <Poster>
+						<LinkElement to={`/movies/${crewMember.id}`}>
+						  <ImagePoster
+							$isCustom={crewMember.poster_path ? false : true}
+							src={
+							  crewMember.poster_path
+								? `https://image.tmdb.org/t/p/original${crewMember.poster_path}`
+								: customPoster
+							}
+							alt={crewMember.title}
+						  ></ImagePoster>
+						</LinkElement>
+					  </Poster>
+					  <TileContent>
+						<LinkElement to={`/movies/${crewMember.id}`}>
+						  <TileTitle>{crewMember.title}</TileTitle>
+						</LinkElement>
+						<Year>({crewMember.release_date ? crewMember.release_date.slice(0, 4) : '-'})</Year>
+						<Tags>
+						  {movieGenres.map((genre) => (
+							<Tag key={genre.id}>{genre.name}</Tag>
+						  ))}
+						</Tags>
+						<Rating>
+						  <StarImage src={star} alt=""></StarImage>
+						  <Rate>{crewMember.vote_average || '-'}</Rate>
+						  <Votes> {crewMember.vote_count ? `${crewMember.vote_count} votes` : '-'}</Votes>
+						</Rating>
+					  </TileContent>
+					</StyledTile>
+				  );
+				})}
+			</Movies>
+		  </Container>
 		</Wrapper>
-	)
-}
+	  );
+	};
 
 export default MovieCrew

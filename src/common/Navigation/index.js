7113b0dfc2d82b78975d6d7d961e2c1fbc940ccd
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-08-21 22:11:59 +0200

    fixed input

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Logo,
   ImageLogo,
@@ -13,27 +13,50 @@ import {
 import video from "../images/Video.svg";
 import Input from "./Input";
 import { toMovies, toPeople } from "../../routes";
+import { useSearchInput } from "./Input/useSearchInput";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 
-export const Navigation = () => (
-  <StyledNav>
-    <Wrapper>
-      <Container>
-        <StyledLink to="/">
-          <Logo>
-            <ImageLogo src={video} alt="logoImg" />
-            Movies Browser
-          </Logo>
-        </StyledLink>
-        <StyledUl>
-          <StyledLi>
-            <StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
-          </StyledLi>
-          <StyledLi>
-            <StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
-          </StyledLi>
-        </StyledUl>
-      </Container>
-      <Input />
-    </Wrapper>
-  </StyledNav>
-);
+export const Navigation = () => {
+  const { newInputValue, onInputChange, isMoviesPage, setNewInputValue } =
+    useSearchInput();
+
+  const resetInputValue = () => {
+    setNewInputValue("");
+  };
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname !== "/movies" &&location.pathname !== "/people") {
+      setNewInputValue("");
+    }
+  }, [location.pathname, setNewInputValue]);
+
+  return (
+    <StyledNav>
+      <Wrapper>
+        <Container>
+          <StyledLink to="/">
+            <Logo>
+              <ImageLogo src={video} alt="logoImg" />
+              Movies Browser
+            </Logo>
+          </StyledLink>
+          <StyledUl>
+            <StyledLi onClick={resetInputValue}>
+              <StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
+            </StyledLi>
+            <StyledLi onClick={resetInputValue}>
+              <StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
+            </StyledLi>
+          </StyledUl>
+        </Container>
+        <Input
+          newInputValue={newInputValue}
+          onInputChange={onInputChange}
+          isMoviesPage={isMoviesPage}
+        />
+      </Wrapper>
+    </StyledNav>
+  );
+}

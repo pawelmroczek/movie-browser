df9751fe7184542bc45b9c2ea81c2f46f6484afa
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-21 18:08:15 +0200

    added link to navigation logo

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Logo,
   ImageLogo,
@@ -13,49 +13,27 @@ import {
 import video from "../images/Video.svg";
 import Input from "./Input";
 import { toMovies, toPeople } from "../../routes";
-import { useSearchInput } from "./Input/useSearchInput";
-import { useLocation } from "react-router-dom/cjs/react-router-dom";
 
-export const Navigation = () => {
-  const { newInputValue, onInputChange, isMoviesPage, setNewInputValue } =
-    useSearchInput();
-
-  const resetInputValue = () => {
-    setNewInputValue("");
-  };
-
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname !== "/movies" &&location.pathname !== "/people") {
-      setNewInputValue("");
-    }
-  }, [location.pathname]);
-
-  return (
-    <StyledNav>
-      <Wrapper>
-        <Container>
+export const Navigation = () => (
+  <StyledNav>
+    <Wrapper>
+      <Container>
+        <StyledLink to="/">
           <Logo>
-            <ImageLogo src={video} alt="" />
+            <ImageLogo src={video} alt="logoImg" />
             Movies Browser
           </Logo>
-          <StyledUl>
-            <StyledLi onClick={resetInputValue}>
-              <StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
-            </StyledLi>
-            <StyledLi onClick={resetInputValue}>
-              <StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
-            </StyledLi>
-          </StyledUl>
-        </Container>
-        <Input
-          newInputValue={newInputValue}
-          onInputChange={onInputChange}
-          isMoviesPage={isMoviesPage}
-        />
-      </Wrapper>
-    </StyledNav>
-  );
-};
-
+        </StyledLink>
+        <StyledUl>
+          <StyledLi>
+            <StyledNavLink to={toMovies()}>MOVIES</StyledNavLink>
+          </StyledLi>
+          <StyledLi>
+            <StyledNavLink to={toPeople()}>PEOPLE</StyledNavLink>
+          </StyledLi>
+        </StyledUl>
+      </Container>
+      <Input />
+    </Wrapper>
+  </StyledNav>
+);

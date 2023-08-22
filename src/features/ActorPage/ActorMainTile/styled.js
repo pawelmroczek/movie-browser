790a2ca1793d5ba91779a5fae4303847d6e3c1e1
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-22 09:46:50 +0200

    added margin between nav and TileWrapper

--- src/features/ActorPage/ActorMainTile/styled.js
+++ src/features/ActorPage/ActorMainTile/styled.js
@@ -23,14 +23,14 @@ export const TileWrapper = styled.div`
 	background-color: ${({ theme }) => theme.color.white};
 	max-width: 1368px;
 	box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
-	margin: 0 auto 21px;
+	margin: 23px auto 21px;
 	display: flex;
 	flex-wrap: wrap;
 
 
 	@media (min-width: 992px) {
 		padding: 40px;
-		margin: 0 auto 64px;
+		margin: 56px auto 64px;
 		display: grid;
 		grid-template-columns: auto 1fr;
 	}

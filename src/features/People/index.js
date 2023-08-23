3ed066a3fd896deab4b48c854f763a0251ea6703
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-23 16:59:17 +0200

    cleared searcher after page refresh

--- src/features/People/index.js
+++ src/features/People/index.js
@@ -36,12 +36,12 @@ export const People = () => {
     }
   }, [page, dispatch, query]);
 
-  // useEffect(() => {
-  // 	dispatch(fetchSearchResult(query));
+  useEffect(() => {
+    dispatch(fetchSearchResult(query));
 
-  // 	const url = window.location.href.split('?')[0];
-  // 	window.history.replaceState({}, document.title, url);
-  //   }, [query, dispatch]);
+    const url = window.location.href.split('?')[0];
+    window.history.replaceState({}, document.title, url);
+  }, [query, dispatch]);
 
   const data = useSelector(selectData);
   const status = useSelector(selectStatus);

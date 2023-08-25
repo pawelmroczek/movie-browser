21215537202f922bf249c7a20cb816394187699a
WerkaStrong <weronika.chmieleska00@gmail.com>
2023-08-25 08:11:45 +0200

    remove changes in ActorPage

--- src/features/ActorPage/index.js
+++ src/features/ActorPage/index.js
@@ -2,41 +2,17 @@ import { Content } from '../Movie/styled'
 import ActorMainTile from './ActorMainTile'
 import MovieCrew from './MoviesCrew'
 import MovieCast from './MoviesCast'
-import { Container } from '../../common/Container'
-import Loader from '../States/Loader'
-import Error from '../States/Error'
-import { useSelector } from 'react-redux'
-import { selectStatus } from '../browserSlice'
 
 const ActorPage = () => {
-
-	const status = useSelector(selectStatus);
-
-	switch (status) {
-		case "loading":
-			return (
-				<Container>
-					<Loader />
-				</Container>
-			)
-		case "error":
-			return (
-				<Container>
-					<Error />
-				</Container>
-			)
-		case "success":
-			return (
-				<>
-					<Content>
-						<ActorMainTile />
-					</Content>
-					<MovieCast />
-					<MovieCrew />
-				</>
-			)
-	}
+	return (
+		<>
+			<Content>
+				<ActorMainTile />
+			</Content>
+			<MovieCast />
+			<MovieCrew />
+		</>
+	)
 }
 
 export default ActorPage
-

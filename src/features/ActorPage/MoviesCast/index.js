c3b42e247cddc187461d28e22c7d89688955e391
Patryk Krawczyk <patryk.krawczyk.it@gmail.com>
2023-08-25 09:35:31 +0200

    Removed redundant code and files

--- src/features/ActorPage/MoviesCast/index.js
+++ src/features/ActorPage/MoviesCast/index.js
@@ -1,53 +1,57 @@
+import { LinkElement, Rate, StarImage, StyledTile, TileTitle, Votes } from '../../../common/Tile/styled'
+import customPoster from '../../../common/images/Video.svg'
+import star from '../../../common/images/Vector.svg'
 import {
-	LinkElement,
-	Rate,
-	StarImage,
-	StyledTile,
-	TileTitle,
-	Votes,
-	Year,
-} from '../../../common/Tile/styled';
-import customPoster from '../../../common/images/Video.svg';
-import star from '../../../common/images/Vector.svg';
-import { Container, Header, Movies, Rating, Tag, Tags, TileContent, Poster, ImagePoster, Character, StyledYear } from './styled';
-import { Wrapper } from '../../Movie/People/styled';
-import { useParams } from 'react-router-dom';
-import useCast from './useCast';
-import { useEffect, useState } from 'react';
-import { getGenres } from '../../../common/Genres/getGenres';
+	Container,
+	Header,
+	Movies,
+	Rating,
+	Tag,
+	Tags,
+	TileContent,
+	Poster,
+	ImagePoster,
+	Character,
+	StyledYear,
+} from './styled'
+import { Wrapper } from '../../Movie/People/styled'
+import { useParams } from 'react-router-dom'
+import useCast from './useCast'
+import { useEffect, useState } from 'react'
+import { getGenres } from '../../../common/Genres/getGenres'
 
 const MovieCast = () => {
-	const [genres, setGenres] = useState([]);
+	const [genres, setGenres] = useState([])
 
 	useEffect(() => {
 		const fetchGenres = async () => {
 			try {
-				const genres = await getGenres();
-				setGenres(genres);
+				const genres = await getGenres()
+				setGenres(genres)
 			} catch (error) {
-				console.error('Error fetching genres:', error);
+				console.error('Error fetching genres:', error)
 			}
-		};
+		}
 
-		fetchGenres();
-	}, []);
+		fetchGenres()
+	}, [])
 
-	const { id } = useParams();
-	const actorId = id;
-	const castData = useCast(actorId);
+	const { id } = useParams()
+	const actorId = id
+	const castData = useCast(actorId)
 
 	if (!castData || castData.length === 0) {
-		return null;
+		return null
 	}
 
 	return (
 		<Wrapper>
 			<Container>
-				<Header> {castData.length != 0 ? `Movies - cast (${castData.length})` : null}</Header>
+				<Header> {castData.length !== 0 ? `Movies - cast (${castData.length})` : null}</Header>
 				<Movies>
 					{castData &&
-						castData.map((castMember) => {
-							const movieGenres = genres.filter((genre) => castMember.genre_ids.includes(genre.id));
+						castData.map(castMember => {
+							const movieGenres = genres.filter(genre => castMember.genre_ids.includes(genre.id))
 
 							return (
 								<StyledTile key={castMember.id}>
@@ -60,8 +64,7 @@ const MovieCast = () => {
 														? `https://image.tmdb.org/t/p/original${castMember.poster_path}`
 														: customPoster
 												}
-												alt={castMember.title}
-											></ImagePoster>
+												alt={castMember.title}></ImagePoster>
 										</LinkElement>
 									</Poster>
 									<TileContent>
@@ -69,30 +72,27 @@ const MovieCast = () => {
 											<TileTitle>{castMember.title}</TileTitle>
 										</LinkElement>
 										<StyledYear>
-											<Character>
-												{castMember.character}
-											</Character>
-											({castMember.release_date ? castMember.release_date.slice(0, 4) : '-'})
+											<Character>{castMember.character}</Character>(
+											{castMember.release_date ? castMember.release_date.slice(0, 4) : '-'})
 										</StyledYear>
 										<Tags>
-											{movieGenres.map((genre) => (
+											{movieGenres.map(genre => (
 												<Tag key={genre.id}>{genre.name}</Tag>
 											))}
 										</Tags>
 										<Rating>
-											<StarImage src={star} alt=""></StarImage>
+											<StarImage src={star} alt=''></StarImage>
 											<Rate>{castMember.vote_average || '-'}</Rate>
 											<Votes> {castMember.vote_count ? `${castMember.vote_count} votes` : '-'}</Votes>
 										</Rating>
 									</TileContent>
 								</StyledTile>
-							);
+							)
 						})}
 				</Movies>
 			</Container>
 		</Wrapper>
-	);
-};
+	)
+}
 
-export default MovieCast;
-
+export default MovieCast
--- src/features/Movies/index.js
+++ src/features/Movies/index.js
@@ -1,115 +1,108 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Container, Content, Title } from "../../common/Container";
-import { Pagination } from "../../common/Pagination";
-import { Tile } from "../../common/Tile";
-import { useDelete, useQueryParameter } from "../../queryParameters";
-import searchQueryParamName from "../../searchQueryParamName";
-import {
-  fetchSearchResult,
-  selectData,
-  selectStatus,
-  selectGenres,
-  fetchPopular,
-} from "../browserSlice";
-import NoResults from "../States/NoResults";
-import Loader from "../States/Loader";
-import Error from "../States/Error";
-import paginationParamName from "../../paginationParamName";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, Content, Title } from '../../common/Container'
+import { Pagination } from '../../common/Pagination'
+import { Tile } from '../../common/Tile'
+import { useQueryParameter } from '../../queryParameters'
+import searchQueryParamName from '../../searchQueryParamName'
+import { fetchSearchResult, selectData, selectStatus, selectGenres, fetchPopular } from '../browserSlice'
+import NoResults from '../States/NoResults'
+import Loader from '../States/Loader'
+import Error from '../States/Error'
+import paginationParamName from '../../paginationParamName'
 
 const Movies = () => {
-  const query = useQueryParameter(searchQueryParamName);
-  const page = useQueryParameter(paginationParamName);
-  const dispatch = useDispatch();
+	const query = useQueryParameter(searchQueryParamName)
+	const page = useQueryParameter(paginationParamName)
+	const dispatch = useDispatch()
 
-  const deleteParam = useDelete();
-  const data = useSelector(selectData);
-  const status = useSelector(selectStatus);
-  const genres = useSelector(selectGenres);
+	const data = useSelector(selectData)
+	const status = useSelector(selectStatus)
+	const genres = useSelector(selectGenres)
 
-  const movies = data.results || [];
-  const totalResults = data.total_results;
+	const movies = data.results || []
+	const totalResults = data.total_results
 
-  useEffect(() => {
-    const payload = {
-      query: query,
-      page: page || 1,
-      destination: "movie",
-    };
-    if (!query) {
-      dispatch(fetchPopular(payload));
-    } else {
-      dispatch(fetchSearchResult(payload));
-    }
-  }, [page, dispatch, query]);
+	useEffect(() => {
+		const payload = {
+			query: query,
+			page: page || 1,
+			destination: 'movie',
+		}
+		if (!query) {
+			dispatch(fetchPopular(payload))
+		} else {
+			dispatch(fetchSearchResult(payload))
+		}
+	}, [page, dispatch, query])
 
-  useEffect(() => {
-    dispatch(fetchSearchResult(query));
+	useEffect(() => {
+		dispatch(fetchSearchResult(query))
 
-    const url = window.location.href.split('?')[0];
-    window.history.replaceState({}, document.title, url);
-  }, [query, dispatch]);
+		const url = window.location.href.split('?')[0]
+		window.history.replaceState({}, document.title, url)
+	}, [query, dispatch])
 
-  switch (status) {
-    case "loading":
-      return query ? (
-        <Container>
-          <Title>Search results for "{query}"</Title>
-          <Loader />
-        </Container>
-      ) : (
-        <Container>
-          <Loader />
-        </Container>
-      );
-    case "error":
-      return (
-        <Container>
-          <Error />
-        </Container>
-      );
-    case "success":
-      if (!movies.length && query) {
-        return (
-          <Container>
-            <Title>Sorry, there are no results for "{query}"</Title>
-            <NoResults />
-          </Container>
-        );
-      } else if (query) {
-        return (
-          <>
-            <Container>
-              <Title>
-                Search results for "{query}" ({totalResults})
-              </Title>
-              <Content>
-                {movies.map((movie) => (
-                  <Tile key={movie.id} movie={movie} genres={genres} />
-                ))}
-              </Content>
-            </Container>
-            <Pagination />
-          </>
-        );
-      } else {
-        return (
-          <>
-            <Container>
-              <Title>Popular movies</Title>
-              <Content>
-                {movies.map((popMovie) => (
-                  <Tile key={popMovie.id} movie={popMovie} genres={genres} />
-                ))}
-              </Content>
-            </Container>
-            <Pagination />
-          </>
-        );
-      }
-    default:
-      return null;
-  }
-};
+	switch (status) {
+		case 'loading':
+			return query ? (
+				<Container>
+					<Title>Search results for "{query}"</Title>
+					<Loader />
+				</Container>
+			) : (
+				<Container>
+					<Loader />
+				</Container>
+			)
+		case 'error':
+			return (
+				<Container>
+					<Error />
+				</Container>
+			)
+		case 'success':
+			if (!movies.length && query) {
+				return (
+					<Container>
+						<Title>Sorry, there are no results for "{query}"</Title>
+						<NoResults />
+					</Container>
+				)
+			} else if (query) {
+				return (
+					<>
+						<Container>
+							<Title>
+								Search results for "{query}" ({totalResults})
+							</Title>
+							<Content>
+								{movies.map(movie => (
+									<Tile key={movie.id} movie={movie} genres={genres} />
+								))}
+							</Content>
+						</Container>
+						<Pagination />
+					</>
+				)
+			} else {
+				return (
+					<>
+						<Container>
+							<Title>Popular movies</Title>
+							<Content>
+								{movies.map(popMovie => (
+									<Tile key={popMovie.id} movie={popMovie} genres={genres} />
+								))}
+							</Content>
+						</Container>
+						<Pagination />
+					</>
+				)
+			}
+		default:
+			return null
+	}
+}
 
-export default Movies;
+export default Movies
